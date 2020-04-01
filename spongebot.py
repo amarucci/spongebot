92e2b01f73d759a208189a52f534c0d6288eab60
Anthony Marucci <amarucci13@gmail.com>
2020-04-01 14:17:29 -0700

    Add feature to spongify text that isn't in another message (no longer has to be in a reply)

--- spongebot.py
+++ spongebot.py
@@ -11,13 +11,18 @@ bot = telebot.TeleBot(TOKEN)
 def send_welcome(message):
     try:
         mix = lambda s: "".join((str.upper,str.lower)[i%2](ch) for i,ch in enumerate(s))
-        new_message = mix(message.reply_to_message.text)
+        if message.reply_to_message is None:
+            new_message = mix(message.text.split(' ', 1)[1])
+            reply_id = message.message_id
+        else:
+            new_message = mix(message.reply_to_message.text)
+            reply_id = message.reply_to_message.message_id
 
         bot.send_photo(
                 message.chat.id, 
                 caption=new_message,
                 photo="https://usatftw.files.wordpress.com/2017/05/spongebob.jpg?w=1000&h=600&crop=1",
-                reply_to_message_id=message.reply_to_message.message_id)
+                reply_to_message_id=reply_id)
     except:
         print("too bad")
 
